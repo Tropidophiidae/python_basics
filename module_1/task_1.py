e7ec43489b88d6b4f181367673d3244933cd7df7
Dzmitry <dzmitry.krupski@outlook.com>
2024-09-17 21:46:56 +0300

    Module 1: Python Basics

--- module_1/task_1.py
+++ module_1/task_1.py
@@ -1,30 +1,67 @@
 # constant for count of elements
 import random
 
-elements_count = 10
+elements_count = 100
 min_random = 0
 max_random = 1000
 
-# create list of 100 random numbers from 0 to 1000
+# TASK: create list of 100 random numbers from 0 to 1000
 # list to store random numbers
-result = []
+random_numbers = []
 
 # cycle to make random list
 for i in range(elements_count):
     # add random value to result list
-    result.append(random.randint(min_random, max_random))
+    random_numbers.append(random.randint(min_random, max_random))
 
-print("Generated list:", result)
+print("Generated list:", random_numbers)
 
-# sort list from min to max (without using sort())
-for i in range(len(result) - 1):
+# TASK: sort list from min to max(without using sort())
+# bubble sort
+for i in range(len(random_numbers) - 1):
+    for j in range(len(random_numbers) - 1 - i):
+        if random_numbers[j] > random_numbers[j + 1]:
+            tmp = random_numbers[j]
+            random_numbers[j] = random_numbers[j + 1]
+            random_numbers[j + 1] = tmp
 
-    for j in range(len(result) - 1 - i):
-        #swap elements in they are in the wrong order
-        if result[j] > result[j + 1]:
-            tmp = result[j]
-            result[j] = result[j + 1]
-            result[j + 1] = tmp
+print("Sorted list:", random_numbers)
 
-print("Sorted list:", result)
+# TASK: calculate average for even and odd numbers
+# list to store even numbers
+even_numbers = []
+# list to store odd numbers
+odd_numbers = []
 
+# Get even and odd numbers and store it in separate lists
+# cycle for all numbers
+for i in random_numbers:
+    # if even -> add to even_numbers
+    if i % 2 == 0:
+        even_numbers.append(i)
+    # if odd (not even) -> add to odd_numbers
+    else:
+        odd_numbers.append(i)
+
+print("Even:", even_numbers)
+print("Odd:", odd_numbers)
+
+
+# Calculate Average for odd
+# function for calculatin average
+def get_average(numbers):
+    # initial result as 0. Did it to use "+="
+    res = 0
+    # for each number in given list
+    for n in numbers:
+        # sum each number
+        res += n
+    # if res still = 0 (in case no numbers) return o, else calculate average
+    return 0 if res == 0 else res / len(numbers)
+
+
+average_even = get_average(even_numbers)
+average_odd = get_average(odd_numbers)
+
+print("Average even:", average_even)
+print("Average odd:", average_odd)
